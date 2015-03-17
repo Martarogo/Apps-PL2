8faa82205e9e891fb3a3905d3b7d63f09e7484c4
Marta Rodríguez <uo216988@uniovi.es>
2015-03-17 18:06:34 +0100

    Lo que había en la dropbox

---
+++ Servidor/Servidor/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Vocabulary;
+
+namespace Servidor
+{
+    class Server
+    {
+        private static int PORT = 23456;
+        private byte[] brec = new byte[128], back;
+        private String crec;
+        private String[] separador = { " | " }, mrec;
+        private int nrec, secrec, sec = 0;
+
+        private UdpClient client = null;
+        BinaryMessageCodec encoding = new BinaryMessageCodec();
+        IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        
+        
+        public void Run()
+        {
+            Console.WriteLine("Servidor en ejecución...");
+
+            try
+            {
+                client = new UdpClient(PORT);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                return;
+            }
+          
+            
+            // El servidor se ejecuta infinitamente
+            for (; ; )
+            {
+                try
+                {
+                    waitMessage();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: {0}", e.Message);
+                }
+
+            }
+        }
+
+        public void waitMessage()
+        {
+            // Recibir información
+            brec = client.Receive(ref remoteIPEndPoint);
+
+            crec = encoding.Decode(brec);
+
+            mrec = crec.Split(separador, StringSplitOptions.RemoveEmptyEntries);
+
+            secrec = int.Parse(mrec[0]);
+            nrec = int.Parse(mrec[1]);
+
+            if (sec != secrec)
+            {
+                Console.WriteLine("El numero de secuencia recibido no conicide. Se descarta el paquete");
+            }
+            else if (sec == secrec)
+            {
+                Message ack = new Message(sec);
+
+                back = encoding.Encode(ack);
+
+                client.Send(back, back.Length, remoteIPEndPoint);
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server serv = new Server();
+            Thread hilo = new Thread(new ThreadStart(serv.Run));
+            hilo.Start();
+            hilo.Join();
+        }
+    }
+}
+
+
---
+++ cliente/cliente/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Vocabulary;
+
+namespace Cliente
+{
+    class Client
+    {
+        private static String SERVER = "localhost";
+        private static int SERVERPORT = 23456;
+        private static int sec = 0, nrec;
+        private String srec;
+        private int[] array_num = { 5, 4, 3, 2, 1 };
+        private byte[] sent, received = new byte[128];
+        BinaryMessageCodec encoding = new BinaryMessageCodec();
+        UdpClient client = null;
+        Message msg;
+
+        public void Run()
+        {
+            try
+            {
+                client = new UdpClient();
+
+                msg = new Message(sec, array_num[0]);
+
+                sendMessage(msg);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+            Console.ReadKey();
+        }
+
+
+        private void sendMessage(Message m) {
+            sent = encoding.Encode(msg);
+
+            client.Send(sent, sent.Length, SERVER, SERVERPORT);
+
+            try
+            {
+                client.Client.ReceiveTimeout = 1000;
+
+                waitACK();
+            }
+            catch (Exception e)
+            {
+                if (e.InnerException != null)
+                {
+                    if (e.InnerException is SocketException)
+                    {
+                        SocketException se = (SocketException)e.InnerException;
+                        if (se.SocketErrorCode == SocketError.TimedOut)
+                        {
+                            Console.WriteLine("Ha expirado el temporizador, se reenvia el mensaje");
+                            sendMessage(msg);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: " + se.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+
+        private void waitACK()
+        {
+            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            // Recibir información
+            received = client.Receive(ref remoteIPEndPoint);
+
+            srec = encoding.Decode(received);
+            nrec = int.Parse(srec);
+
+            if (sec != (nrec / 10))
+            {
+                //Enviar mensaje
+            }
+            else if (sec == (nrec / 10))
+            {
+                sec++;
+            }
+            Console.WriteLine("Sec: " + sec);
+        }
+    }
+
+    
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 1;
+
+            Thread[] threads = new Thread[N];
+            for (int i = 0; i < N; i++)
+            {
+                Client client = new Client();
+                threads[i] = new Thread(new ThreadStart(client.Run));
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                threads[i].Join();
+            }
+
+        }
+    }
+}
